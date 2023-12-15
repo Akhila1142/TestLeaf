97f9f3c1575c5bc27d59af7b725aeef1d6d86dc3
Akhila1142 <regondaakhila26@gmail.com>
2023-12-14 23:58:29 -0500

    Chakri changed few things

--- Selenium/src/main/java/week1/day2/evenNumber.java
+++ Selenium/src/main/java/week1/day2/evenNumber.java
@@ -1,7 +1,7 @@
 package week1.day2;
 
 public class evenNumber {
-public static void main(String[] args) {
-	System.out.println("hello second program");
-}
+	public static void main(String[] args) {
+		System.out.println("this is chakri changing the things");
+	}
 }
